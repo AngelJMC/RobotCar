29fd1d5e9f3adcfffbcf7b6940207c8a8aca0bed
Angel Maldonado <angelgesus@gmail.com>
2018-06-16 16:14:10 +0200

    Added Infra-red library sensors
    Added new library to read the infra-red sensors array

---
+++ Firmware/lib/ROBC_ObstaclesDetection/ROBC_IRsensors.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include <ROBC_IRsensors.h>
+
+ROBC_IRsensors::ROBC_IRsensors( void)
+{
+  _irData = NULL;
+}
+
+void ROBC_IRsensors::init(const int irN, const analog_inputs_t *irInputs)
+{
+
+  _nSensors = irN;
+  _irData = new ir_t[_nSensors];
+
+for(int ir=0; ir<irN; ++ir){
+  _irData[ir].irInputs = irInputs[ir];
+
+}
+}
+
+
+void ROBC_IRsensors::update( void )
+{
+  for( int ir=0; ir<_nSensors; ++ir){
+      _irData[ir].ir_val = analogRead( _irData[ir].irInputs);
+
+  }
+
+}
+
+void ROBC_IRsensors::print( void )
+{
+  for( int ir=0; ir<_nSensors; ++ir){
+    Serial.print(_irData[ir].ir_val);
+    Serial.print(", ");
+  }
+  Serial.print("\r");
+}
