7d1bcf7bd9ce2df27307d9782eda8f01715403a6
Angel Maldonado <angelgesus@gmail.com>
2018-06-22 20:07:26 +0200

    Testing rosserial communication

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -3,6 +3,12 @@
 #include <I2C.h>
 #include <Arduino_Mega_ISR_Registry.h>
 #include <AP_PeriodicProcess.h>
+// #include <ros.h>
+// #include <std_msgs/String.h>
+// #include <geometry_msgs/Twist.h>
+// #include <std_msgs/UInt16MultiArray.h>
+// #include <std_msgs/UInt16.h>
+// #include <std_msgs/Float32.h>
 #include <AP_InertialSensor.h>
 #include <AP_Math.h>
 #include <AP_Common.h>
@@ -11,10 +17,14 @@
 #include <ROBC_motorControl.h>
 #include <ROBC_IRsensors.h>
 
+
 #include "Servo.h"
 #include <APM_RC.h> // ArduPilot Mega RC Library
 #include "cli.h"
 #include "rc_command.h"
+
+
+
 
 #define APM2_HARDWARE
 #define MPUREG_PRODUCT_ID_X                               0x0C    // Product ID Register
@@ -47,6 +57,22 @@ cliHdlr_t _cliHdlr={
   .FlagBufferInput = false,  // whether the string is complete
 };
 
+// ros::NodeHandle  nh;
+// std_msgs::String str_msg;
+// ros::Publisher chatter("chatter", &str_msg);
+//
+// std_msgs::Float32 temp_msg;
+// ros::Publisher pub_temp("temperature", &temp_msg);
+//
+// geometry_msgs::Twist imu_msg;
+// ros::Publisher pub_imu("imu", &imu_msg);
+//
+// std_msgs::UInt16MultiArray ir_msg;
+// ros::Publisher pub_ir("ir_sens", &ir_msg);
+
+
+// char hello[13] = "hello world!";
+
 delaysMs_t lastMs;
 
 Arduino_Mega_ISR_Registry _isr_registry,_isr_registry1;
@@ -58,8 +84,21 @@ Servo servoSteering ;  // create servo object to control a servo
 motorControl motorCtrl(Kp, Ki, Kd);
 ROBC_IRsensors IRsensors;
 
+// uint16_t test[5]={1,2,3,4,5};
 
-void serialEvent();
+// void serialEvent();
+
+// void servo_cb( const std_msgs::UInt16& cmd_msg){
+//   refVal.servoVal = cmd_msg.data;
+// }
+//
+// void speed_cb( const std_msgs::UInt16& cmd_msg){
+//   refVal.speedVal = cmd_msg.data;
+// }
+//
+// ros::Subscriber<std_msgs::UInt16> sub_servo("servo", servo_cb);
+// ros::Subscriber<std_msgs::UInt16> sub_speed("speed", speed_cb);
+
 
 
 static void flash_leds(bool on)
@@ -73,6 +112,16 @@ static void flash_leds(bool on)
 
 void setup()
 {
+
+  // nh.initNode();
+  // nh.advertise(chatter);
+  // nh.advertise(pub_temp);
+  // nh.advertise(pub_imu);
+  // nh.advertise(pub_ir);
+  //
+  //  nh.subscribe(sub_servo);
+  //  nh.subscribe(sub_speed);
+
   I2c.begin();
   I2c.timeOut(20);
   I2c.setSpeed(true);
@@ -83,7 +132,9 @@ void setup()
   SPI.begin();
 
   SPI.beginTransaction (SPISettings (1000000, MSBFIRST, SPI_MODE3));
-  Serial.begin(115200);
+  Serial.begin(57600);
+
+
 
   delay(1);
   pinMode(BLUE_LED_PIN, OUTPUT);
@@ -102,8 +153,15 @@ void setup()
   initRC(&_rcPPM, &_isr_registry);
   servoSteering.attach(SERVO_INPUT);
   motorCtrl.initialize();
+
+
+
   _cliHdlr.BufferSerialInput.reserve(20);   // reserve 20 bytes for the inputString:
   refVal.servoVal = 100;
+  refVal.speedVal = 0;
+
+  // ir_msg.data_length = 5;
+
 }
 
 
@@ -119,37 +177,66 @@ void loop()
   }
 
   if ( (millis() - lastMs.sensors) >=10){
+
     Vector3f accel;
     Vector3f gyro;
     lastMs.sensors = millis();
     ins.update();
+    IRsensors.update();
 
     accel = ins.get_accel();
     gyro = ins.get_gyro();
 
-    Serial.print(accel.x);
-    Serial.print("  ,  ");
-    Serial.print(accel.y);
-    Serial.print("  ,  ");
-    Serial.println(accel.z);
-    Serial.print(gyro.x);
-    Serial.print("  ,  ");
-    Serial.print(gyro.y);
-    Serial.print("  ,  ");
-    Serial.println(gyro.z);
+    // temp_msg.data = accel.x;
+    // pub_temp.publish(&temp_msg);
+    //
+    // imu_msg.linear.x = accel.x;
+    // imu_msg.linear.y = accel.y;
+    // imu_msg.linear.z = accel.z;
+    //
+    // imu_msg.angular.x = gyro.x;
+    // imu_msg.angular.y = gyro.y;
+    // imu_msg.angular.z = gyro.z;
+    //
+    // pub_imu.publish(&imu_msg);
 
-    IRsensors.update();
-    IRsensors.print();
+    // Serial.print(accel.x);
+    // Serial.print("  ,  ");
+    // Serial.print(accel.y);
+    // Serial.print("  ,  ");
+    // Serial.println(accel.z);
+    // Serial.print(gyro.x);
+    // Serial.print("  ,  ");
+    // Serial.print(gyro.y);
+    // Serial.print("  ,  ");
+    // Serial.println(gyro.z);
+
+    // for(uint8_t irIndex; irIndex < IR_SENSORS_N; ++irIndex){
+    //   test[irIndex]  = IRsensors.getIR(irIndex);
+    // }
+
+    // ir_msg.data = test;
+    // pub_ir.publish(&ir_msg);
+    // IRsensors.print();
   }
+
+  // if ((millis() - lastMs.steering) >= 500) {
+  //   lastMs.steering = millis();
+  //
+  //   str_msg.data = hello;
+  //   chatter.publish( &str_msg );
+  //
+  // }
+  // nh.spinOnce();
 
   if ((millis() - lastMs.motor) >= 25) {
 
     lastMs.motor = millis();
     motorCtrl.updateSpeed(refVal.speedVal);
-    motorCtrl.printInfoPID();
-
-    lastMs.steering = millis();
     servoSteering.write( refVal.servoVal );
+
+     motorCtrl.printInfoPID();
+
   }
 }
 
